de27852e0e6c39075158c5ab07ca73a2150078f2
DangGiaThuan <danggiathuanhl@gmail.com>
2024-10-26 15:15:42 +0700

    first

--- jobportal_Client/src/redux/JobPost/jobPost.reducer.js
+++ jobportal_Client/src/redux/JobPost/jobPost.reducer.js
@@ -1,31 +1,40 @@
 import { GET_ALL_JOB_FAILURE, GET_ALL_JOB_REQUEST, GET_ALL_JOB_SUCCESS } from "./jobPost.actionType"
 
 const initialState = {
-    post: null, // Bài đăng công việc hiện tại
-    loading: false, // Trạng thái tải
-    error: null, // Lỗi nếu có
+    post: null,
+    loading: false,
+    error: null,
     jobPost: [], // Mảng lưu trữ các bài đăng công việc
-    approve: false // Trạng thái phê duyệt công việc
-}
+    totalPages: 0, // Tổng số trang
+    approve: false
+};
+
 
 export const jobPostReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_ALL_JOB_REQUEST:
-            return { ...state, error: null, loading: false }
-        case GET_ALL_JOB_SUCCESS:
-        case GET_ALL_JOB_FAILURE:
-        // case CREATE_POST_REQUEST:
-        // case GET_ALL_POST_REQUEST:
-        // case LIKE_POST_REQUEST:
-        //     return { ...state, error: null, loading: false }
+            return { 
+                ...state, 
+                loading: true, // Bắt đầu trạng thái tải
+                error: null // Đặt lỗi về null
+            };
 
         case GET_ALL_JOB_SUCCESS:
             return { 
                 ...state, 
-                jobPost: action.payload, // Lưu trữ tất cả các công việc vào mảng jobPost
-                loading: false, 
-                error: null 
-            }
+                jobPost: action.payload.content, // Lưu trữ tất cả các công việc vào mảng jobPost
+                totalPages: action.payload.page.totalPages, // Lưu trữ tổng số trang
+                loading: false, // Kết thúc trạng thái tải
+                error: null // Đặt lỗi về null
+            };
+
+        case GET_ALL_JOB_FAILURE:
+            return { 
+                ...state, 
+                loading: false, // Kết thúc trạng thái tải
+                error: action.payload // Lưu trữ lỗi nếu có
+            };
+
         // case GET_ALL_POST_SUCCESS:
         //     return {
         //         ...state,
